56c8577e01f142183d56df45ab039b3419d5c0dd
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-11-17 10:17:34 +0100

    first comment

--- App.tsx
+++ App.tsx
@@ -4,17 +4,38 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.blue}>Hello World!</Text>
+      <Text style={styles.bigRed}>How are you?</Text>
+      <Text style={[styles.blue, styles.bigRed]}>I'm feeling blue!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightgreen',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+
+  blue: {
+  color: 'blue',
+  fontWeight: '600',
+  },
+
+  bigRed: {
+  color: 'red',
+  fontSize: 30,
+  },
+
+  bigRedBold: {
+  color: 'red',
+  fontSize: 30,
+  fontWeight: '600',
+},
+  });
+
+
