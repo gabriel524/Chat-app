c36af256bbf1ec82af035926983989c116735fb0
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-11-18 23:01:03 +0100

    file update

--- components/Chat.js
+++ components/Chat.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import { GiftedChat } from "react-native-gifted-chat";
 
 export default class Chat extends Component {
-  componentDidMount() {
-    const name = this.props.route.params.name;
-    this.props.navigation.setOptions({ title: name });
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    };
   }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: "Hello developer",
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: "React Native",
+            avatar: "https://placeimg.com/140/140/any",
+          },
+        },
+      ],
+    });
+  } 
+  
   render() {
     const backgroundColor = this.props.route.params.color;
     return (
