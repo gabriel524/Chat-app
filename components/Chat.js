0787999cba5a887729243522de72ace7ef911fc0
Gabriel Abraham Singbeh <68817096+gabriel524@users.noreply.github.com>
2022-11-22 23:43:03 +0100

    file update

--- components/Chat.js
+++ components/Chat.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { StyleSheet, View, Text, Platform, KeyboardAvoidingView } from "react-native";
 import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
-
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import NetInfo from "@react-native-community/netinfo";
 import CustomActions from "./CustomActions";
@@ -10,8 +9,7 @@ import MapView from "react-native-maps";
 
 const firebase = require("firebase");
 require("firebase/firestore");
-
- class Chat extends React.Component {
+export default class Chat extends React.Component {
   constructor() {
     super();
     this.state = {
@@ -41,7 +39,7 @@ require("firebase/firestore");
     if (!firebase.apps.length) {
       firebase.initializeApp(firebaseConfig);
     }
-    
+
     this.referenceChatMessages = firebase.firestore().collection("messages");
   }
 
@@ -58,31 +56,6 @@ require("firebase/firestore");
       console.log(error.message);
     }
   }
-
-  //Retrieve collection data & store in messages
-  onCollectionUpdate = (querySnapshot) => {
-    const messages = [];
-    // Go through each document
-    querySnapshot.forEach((doc) => {
-      // Get QueryDocumentSnapshot's data
-      let data = doc.data();
-      messages.push({
-        _id: data._id,
-        text: data.text || "",
-        createdAt: data.createdAt.toDate(),
-        user: {
-          _id: data.user._id,
-          name: data.user.name,
-          avatar: data.user.avatar || "",
-        },
-        image: data.image || null,
-        location: data.location || null,
-      });
-    });
-    this.setState({
-      messages,
-    });
-  };
 
   componentDidMount() {
     // Set the name property to be included in the navigation bar
@@ -111,7 +84,7 @@ require("firebase/firestore");
                 createdAt: new Date(),
                 user: {
                   _id: 2,
-                  name: "React Native",
+                  name: "Gabriel",
                   avatar: "https://placeimg.com/140/140/any",
                 },
               },
@@ -155,6 +128,7 @@ require("firebase/firestore");
       }),
       () => {
         this.addMessage();
+        this.saveMessages();
       }
     );
   }
@@ -171,6 +145,28 @@ require("firebase/firestore");
       location: message.location || null,
     });
   };
+
+  async saveMessages() {
+    try {
+      await AsyncStorage.setItem(
+        "messages",
+        JSON.stringify(this.state.messages)
+      );
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
+  async deleteMessages() {
+    try {
+      await AsyncStorage.removeItem("messages");
+      this.setState({
+        messages: [],
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
 
   //Allows bubble customization
   renderBubble(props) {
@@ -191,6 +187,30 @@ require("firebase/firestore");
       />
     );
   }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const messages = [];
+    // go through each document
+    querySnapshot.forEach((doc) => {
+      // get the QueryDocumentSnapshot's data
+      let data = doc.data();
+      messages.push({
+        _id: data._id,
+        text: data.text,
+        createdAt: data.createdAt.toDate(),
+        user: {
+          _id: data.user._id,
+          name: data.user.name,
+          avatar: data.user.avatar || "",
+        },
+        image: data.image || null,
+        location: data.location || null,
+      });
+    });
+    this.setState({
+      messages,
+    });
+  };
 
   renderInputToolbar(props) {
     if (this.state.isConnected == false) {
@@ -223,7 +243,7 @@ require("firebase/firestore");
 
   render() {
     // Set the color property as background color for the chat screen
-    const{color, name} = this.props.route.params.color;
+    let color = this.props.route.params.color;
     return (
       <ActionSheetProvider>
         <View style={[styles.container, { backgroundColor: color }]}>
@@ -233,17 +253,17 @@ require("firebase/firestore");
             messages={this.state.messages}
             renderInputToolbar={this.renderInputToolbar.bind(this)}
             onSend={(messages) => this.onSend(messages)}
-            user={{
-              _id: this.state.user._id,
-              name: name,
-            }}
             renderActions={this.renderCustomActions.bind(this)}
             renderCustomView={this.renderCustomView.bind(this)}
+            user={{
+              _id: this.state.uid,
+              avatar: "https://placeimg.com/140/140/any",
+            }}
           />
           {Platform.OS === "android" ? (
             <KeyboardAvoidingView behavior="height" />
           ) : null}
-        </View> 
+        </View>
       </ActionSheetProvider>
     );
   }
@@ -254,5 +274,3 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
-
-export default Chat;
